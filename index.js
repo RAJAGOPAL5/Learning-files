10d662e1dca104dd6bc1e09010b4b3383cd9c18d
RAJAGOPAL5 <95209764+RAJAGOPAL5@users.noreply.github.com>
2022-04-29 11:52:44 +0530

    Blackjack
    Blackjack

--- index.js
+++ index.js
@@ -1,16 +1,96 @@
-function saveLead(){
-    console.log("button clicked")
-    // return "button clicked";
+    let cards = [];
+    let sum = 0;
+    let hasBlackJack = false;
+    let isAlive = false;
+    let message = " "
+    let messageEl = document.getElementById("message-el");
+    let cardsEl = document.getElementById("cards-el");
+    let sumEl = document.getElementById("sum-el")
+
+
+let player = {
+
+    name: "Raja",
+    chips: "25"
+}
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+
+
+    function getRandomCard () {
+        return Math.floor(Math.random()*13) + 1;
+        if (randomNumber > 10){
+            return 10
+        }    
+        else if(randomNumber ===1){
+            return 11
+        }
+        else{
+            randomNumber
+        }
+    }
+
+   
+
+
+
+////////////////////////////////////////////////
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    let cards = [firstCard , secondCard];
+    let sum = firstCard + secondCard;
+    renderGame()
 }
 
-let practiceEl = document.getElementById("practice-el");
-practiceEl.addEventListener("click",function()
-{
-    console.log("button clicked without html code");
-})
 
-let boxEl = document.getElementById("box");
-boxEl.addEventListener("click",function()
-{
-    console.log("open box button clicked using the add eventv listner");    
-})
+///////////////////////////////////////////////////
+
+
+ function renderGame() {
+
+    cardsEl.innerText = "Cards: " + cards[0] + " + " + cards[1];
+    for (let i = 0;i < cards.length; i++ )
+    {
+        cardsEl.textContent += cards[i] + "  "
+    }        
+    
+    sumEl.innerText = "Sum:" + " " +  sum
+
+    if (sum < 21)
+    {
+    message = "Do You Want to draw a new Card?"
+    }
+    else if (sum === 21) 
+    {
+    message = "You Have got BlackJack"
+    hasBlackJack = true;    
+    } 
+    else 
+    {
+    message = "You're Out of the game"    
+    isAlive = false;    
+}
+messageEl.innerText = message;
+}
+
+
+
+function newCard(){
+    // console.log("Draw a new card from the deck");
+    if(isAlive === true && hasBlackJack === false)
+    {
+        let card = getRandomCard()  ;
+    sum = sum + card;
+    cards.push(card);
+    console.log(cards);
+    renderGame()
+
+    }
+}   
+
+
+    
